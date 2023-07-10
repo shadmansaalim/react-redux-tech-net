348d9b40c2fdbe93a712c1aac73bdc7eaa592316
shadmansaalim <shadmansaalim999@gmail.com>
2023-07-10 22:03:40 +0600

    Understanding actions,dispatch,payload

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import type { RootState } from './redux/store'
+import { useSelector, useDispatch } from 'react-redux'
+import {increment, decrement,incrementByAmount } from './redux/features/counter/counterSlice'
 
 function App() {
+  const {count} = useSelector((state: RootState) => state.counter)
+  const dispatch = useDispatch()
+
 
   return (
-    <h1 className="bg-red-500">Tech net</h1>
+    <div>
+    <div className="flex justify-center items-center">
+      <button 
+      className="bg-green-500 p-2 m-2"
+      aria-label="Increment value"
+      onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      <button 
+      className="bg-green-500 p-2 m-2"
+      aria-label="Increment value"
+      onClick={() => dispatch(incrementByAmount(5))}>
+        Increment by 5
+      </button>
+      <div>{count}</div>
+      <button 
+      className="bg-red-500 p-2 m-2"
+      aria-label="Decrement value"
+      onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+    </div>
+  </div>
   )
 }
 
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
-export interface ICounterState {
+export interface CounterState {
   count: number;
 }
 
-const initialState: ICounterState = {
+const initialState: CounterState = {
   count: 0,
 };
 
@@ -18,5 +19,13 @@ const counterSlice = createSlice({
     decrement: (state) => {
       state.count -= 1;
     },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.count += action.payload;
+    },
   },
 });
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
