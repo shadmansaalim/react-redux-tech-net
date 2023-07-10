b9c464f501d2539e1d8114ad5c907dafc9d5cd90
shadmansaalim <shadmansaalim999@gmail.com>
2023-07-10 23:55:17 +0600

    Middleware and custom middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
+// import logger from "redux-logger";
+
+// Custom middleware
+import logger from "./middlewares/logger";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
