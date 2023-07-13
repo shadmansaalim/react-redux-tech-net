57feaab31e20279262348cb8972a1c085492eba1
shadmansaalim <shadmansaalim999@gmail.com>
2023-07-13 13:40:57 +0600

    Redux store created and connected

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import routes from './routes/routes.tsx';
+import routes from './routes/routes';
+import store from './redux/store';
+import {Provider} from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={routes} />
+    </Provider>
   </React.StrictMode>
 );
