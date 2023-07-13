031f6aaa49009b672211464aa415ae473515a97d
shadmansaalim <shadmansaalim999@gmail.com>
2023-07-13 13:35:16 +0600

    Frontend web UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-import {increment, decrement,incrementByAmount } from './redux/features/counter/counterSlice'
-import { useAppDispatch, useAppSelector } from './redux/hooks'
+import { Toaster } from './components/ui/Toaster';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
-  const {count} = useAppSelector(state => state.counter)
-  const dispatch = useAppDispatch()
-
-
   return (
     <div>
-    <div className="flex justify-center items-center">
-      <button 
-      className="bg-green-500 p-2 m-2"
-      aria-label="Increment value"
-      onClick={() => dispatch(increment())}>
-        Increment
-      </button>
-      <button 
-      className="bg-green-500 p-2 m-2"
-      aria-label="Increment value"
-      onClick={() => dispatch(incrementByAmount(5))}>
-        Increment by 5
-      </button>
-      <div>{count}</div>
-      <button 
-      className="bg-red-500 p-2 m-2"
-      aria-label="Decrement value"
-      onClick={() => dispatch(decrement())}>
-        Decrement
-      </button>
+      <Toaster />
+      <MainLayout />
     </div>
-  </div>
-  )
+  );
 }
 
-export default App
+export default App;
